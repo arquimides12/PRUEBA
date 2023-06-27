44f50ffd09e27f2de6278c1d9a58846cbeaa07d2
arquimides12 <137929297+arquimides12@users.noreply.github.com>
2023-06-27 13:38:10 -0500

    Add files via upload

---
+++ JAVAagroeve/practica1/src/practica1esctructura/DoWhile.java
@@ -0,0 +1,14 @@
+package practica1esctructura;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Detector variable
+		int c=1;
+do {
+	System.out.println(c);
+	c++;
+}while(c<=5);
+		}
+}
---
+++ JAVAagroeve/practica1/src/practica1esctructura/For.java
@@ -0,0 +1,10 @@
+package practica1esctructura;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
